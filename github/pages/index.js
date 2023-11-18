2be9fee112c85e264eedb8963e258286a4e01309
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-18 16:56:25 +0330

    show page for authenticated user by useSession

--- github/pages/index.js
+++ github/pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { status } = useSession();
   const signInHandler = () => {
     signIn("github");
   };
@@ -22,14 +23,20 @@ export default function Home() {
       </Head>
       <main>
         <h1>Next Auth | GitHub </h1>
-        <button onClick={signInHandler}>Sign In</button>
-        <button onClick={signOutHandler}>Sign Out</button>
-        <button>
-          <Link href="/dashbord">Dashbord</Link>
-        </button>
-        <button>
-          <Link href="/ssrdashbord">SSR Dashbord</Link>
-        </button>
+        {status === "unauthenticated" ? (
+          <button onClick={signInHandler}>Sign In</button>
+        ) : null}
+        {status === "authenticated" ? (
+          <>
+            <button onClick={signOutHandler}>Sign Out</button>
+            <button>
+              <Link href="/dashbord">Dashbord</Link>
+            </button>
+            <button>
+              <Link href="/ssrdashbord">SSR Dashbord</Link>
+            </button>
+          </>
+        ) : null}
       </main>
     </>
   );
